cf8cfe704b3302a45e7f5b1741a73a67719e194d
Alexandre de Freitas Souza <alexandrefsoficial@gmail.com>
2023-05-09 20:42:52 -0300

    first commit

---
+++ src/controllers/events-controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import eventsService from '@/services/events-service';
+
+export async function getDefaultEvent(_req: Request, res: Response) {
+  try {
+    const event = await eventsService.getFirstEvent();
+    return res.status(httpStatus.OK).send(event);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(httpStatus.NOT_FOUND).send({});
+  }
+}
